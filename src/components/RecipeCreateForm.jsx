bf16d9c659ec814dab37c5b92ad64154e82b72fd
GianlorenzoDercole <johndercole01@gmail.com>
2022-11-14 12:16:19 -0800

    added controlled form see recipecreatefrom.jsx

---
+++ src/components/RecipeCreateForm.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+export default function RecipeForm({submitHandler, initialForm}) {
+    const [form, setForm] = useState(initialForm)
+    return (
+        <form onSubmit={e => submitHandler(e, form, setForm)}>
+
+            <label htmlFor='recipe' className='inputLabel'> recipe </label>
+            <input
+                type='text'
+                id='recipe'
+                value={form.recipe}
+                onChange={e => setForm({...form, recipe: e.target.value})
+                }
+            />
+            <label htmlFor='ingredients' className='inputLabel'> ingredients </label>
+            <input
+                type='text'
+                id='ingredients'
+                value={form.ingredients}
+                onChange={e => setForm({...form, ingredients: e.target.value})
+                }
+            />
+
+            <label htmlFor='instructions' className='inputLabel'> instructions </label>
+            <input
+                type='text'
+                id='instructions'
+                value={form.instructions}
+                onChange={e => setForm({...form, instructions: e.target.value})
+                }
+            />
+            <div>
+                <button type='submit'>update</button>
+            </div>
+        </form>
+    )
+}
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import RecipeForm from '../RecipeCreateForm'
 export default function Home() {
     console.log('my server: ', process.env.REACT_APP_SERVER_URL)
     const [recipes, setRecipes] = useState([])
@@ -20,17 +21,48 @@ export default function Home() {
         fetchRecipes()
     }, [])  // get recipes when page loads
 
+    const handleSubmit = async (e, form, setForm) => {
+        e.preventDefault()
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/recipes`, form)
+
+            setRecipes([...recipes, response.data])
+
+            setForm({
+                recipe: '',
+                ingredients:[],
+                instructions: ''
+            })
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
+
     const recipeLinks = recipes.map((recipe, idx) => {
         return (
+
+
             <div key={`recipelinks${idx}`}>
                 <Link to={`/recipes/${recipe._id}`}>{recipe.recipe}</Link>
             </div>
+
         )
     })
     return (
         <div>
             <h1>app</h1>
+            <RecipeForm
+                submitHandler={handleSubmit}
+                initialForm={{
+                    recipe: '',
+                    ingredients:[],
+                    instructions: ''
+
+                }}
+            />
             {recipeLinks}
+
         </div>
     )
 }
