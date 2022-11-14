1657d6979ed96a23288189701cd4a6e130a688e3
GianlorenzoDercole <johndercole01@gmail.com>
2022-11-14 13:59:43 -0800

    saved new ingredient in state rendering ingredient see recipe,jsx

--- src/components/pages/Recipe.jsx
+++ src/components/pages/Recipe.jsx
@@ -5,20 +5,59 @@ import axios from 'axios'
 export default function Recipe() {
     const [recipe, setRecipe] = useState({})
     const { id } = useParams()
-
-        useEffect(() => {
-            axios.get(`${process.env.REACT_APP_SERVER_URL}/recipes/${id}`)
-
-                .then(response => {
-                    setRecipe(response.data)
-                })
+    const [ingredient, setIngredient] = useState({
+        ingredient: '',
+        amount: 5
+    })
 
 
+
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_SERVER_URL}/recipes/${id}`)
+
+            .then(response => {
+                setRecipe(response.data)
+            })
 
         }, [id])
+
+
+        console.log(ingredient)
+
+        // const handleSubmit = async (e, ingredient, setIngredient) => {
+        //     e.preventDefault()
+        //     try {
+        //         const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/recipes`, form)
+
+        //         setRecipes([...recipes, response.data])
+
+        //         setForm({
+        //             recipe: '',
+        //             ingredients:[],
+        //             instructions: ''
+        //         })
+        //     } catch (err) {
+        //         console.log(err)
+        //     }
+
+        // }
     return (
+
         <div>
-            { <RecipeDetails recipe={recipe}/> }
+            <form onSubmit={e => e.preventDefault()}>
+                <label htmlFor='ingredient' className='inputLabel'> new ingredient </label>
+                <input
+                    type='text'
+                    id='ingredient'
+                    value={ingredient.ingredient}
+                    onChange={e => setIngredient({...ingredient, ingredient:e.target.value})}
+                />
+                <div>
+                    <button type='submit'>update</button>
+                </div>
+            </form>
+             <RecipeDetails recipe={recipe}/>
+             <h1>{ingredient.ingredient}</h1>
         </div>
     )
 }
